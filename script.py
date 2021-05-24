b422d0ba3c804c5ec624ba0270c0aa23ca055758
DK-Creation-DK <81550566+DK-Creation-DK@users.noreply.github.com>
2021-05-24 08:40:07 +0000

    Update script.py

--- script.py
+++ script.py
@@ -9,6 +9,8 @@ Bot simply search for the files from provided channel according to given query a
 
 So no need to add filters for your files or movies from now on!
 
+Join @Viruzhex 
+
 For more click <i>help</i></b>"""
 
 
@@ -26,17 +28,17 @@ No need add each filter again!
 Bot will automatically search for your file and give links to that!
 </i>
 
-<b>© @TroJanzHEX</b>"""
+<b>© @Viruzhex</b>"""
 
 
     ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot</b>
     
-⭕️<b>Creater :</b> @TroJanzHEX
+⭕️<b>Creater :</b> @Viruzhex
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
+⭕️<b>YouTube Channel :</b> <a href='https://youtube.com/channel/UC9CADKE_PnqPgAxc1yJ4R_g'>Channel Link</a> 
 
 """
